2a1041e7f44b02e9b9f4ebaf78369225489dfc4f
Anas <anasnaseer14@gmail.com>
2026-03-23 01:34:55 +0500

    cleaned up

--- core/imperative_shell.py
+++ core/imperative_shell.py
@@ -8,51 +8,6 @@ import hashlib
 import time
 
 
-#FUNCTIONS GIVEN IN ZE PROJECT FOLDER BY ZE SIR
-
-# def generate_signature(raw_value_str: str, key: str, iterations: int) -> str:
-#     """
-#     Generates a PBKDF2 HMAC SHA-256 signature for the given value.
-#     Treats the secret key as the password and the raw value as the salt.
-#     """
-#     password_bytes = key.encode('utf-8')
-#     salt_bytes = raw_value_str.encode('utf-8')
-    
-#     # Generate the hash
-#     hash_bytes = hashlib.pbkdf2_hmac(
-#         hash_name='sha256', 
-#         password=password_bytes, 
-#         salt=salt_bytes, 
-#         iterations=iterations
-#     )
-#     return hash_bytes.hex()
-
-# def Authenticator(config: Dict, metric_value):
-#     SECRET_KEY = config["processing"]["stateless_tasks"].get("secret_key")
-#     ITERATIONS = config["processing"]["stateless_tasks"].get("iterations")
-#     rounded_value = str(round(metric_value,2))
-#     return generate_signature(rounded_value,SECRET_KEY,ITERATIONS)
-
-
-
-
-
-
-
-# #THE MAIN FUNCTION
-# #THIS ALSO ACTS AS THE IMPERATIVE SHELL
-# #THE IMPURE TING
-
-# def run(config: Dict, InputQueue: Queue, VerifiedQueue: Queue, OutputQueue: Queue) -> None:
-#     while True:
-#         packet = InputQueue.get()
-#         if packet is None:
-#             break
-
-#         if Authenticator(config, packet.get('metric_value')) == packet.get('security_hash'):
-#             time.sleep(0.05)
-#             VerifiedQueue.put(packet)
-
 
 class ImperativeShell:
     def __init__(self,config,VerifiedQueue,OutputQueue,InputQueue):
--- input.py
+++ input.py
@@ -4,46 +4,6 @@ import time
 import csv
 
 
-# def MapToInternal(config: Dict, original_column_name: str,original_value: Any, columnsFromConfig: List[Dict]) -> Tuple[str,Any]:
-    
-#     #in config.json/schema_mapping there exists a list of dictionaries called columns
-#     #The line below filters those dictionaries to get the one that we need to map our current column
-#     #to the internal one specified in config.json
-#     column = next(filter(lambda x: x.get('source_name') == original_column_name, columnsFromConfig))
-
-#     required_data_type = column.get('data_type')
-#     if required_data_type == "string":
-#         casted_value = str(original_value)
-#     elif required_data_type == "integer":
-#         casted_value = int(original_value)
-#     elif required_data_type == "float":
-#         casted_value = float(original_value)
-
-#     #Here we return the internal column name and the typecasted value
-#     return column.get('internal_mapping'), casted_value
-
-#     #Might have to add exception handling in this function.
-
-# def run(config: Dict, InputQueue: Queue) -> None:
-#     try:
-#         with open(config.get('dataset_path'), 'r') as DataFile:
-#             csv_reader = csv.DictReader(DataFile)
-#             columnsFromConfig: List[Dict] = config["schema_mapping"].get('columns')
-        
-#             rename = lambda x: {MapToInternal(config,k,v,columnsFromConfig)[0]:MapToInternal(config,k,v,columnsFromConfig)[1] for k,v in x.items()}
-
-
-#             sleep_time = config['pipeline_dynamics'].get('input_delay_seconds')
-#             for row in csv_reader:
-#                 packet: Dict = rename(row)
-#                 InputQueue.put(packet)
-#                 time.sleep(sleep_time)
-
-#     except FileNotFoundError:
-#         print('File not found!')
-#     except csv.Error:
-#         print("Something went wrong while reading data")
-
 
 class InputProcessor:
     def __init__(self,config,InputQueue):
